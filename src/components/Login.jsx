2d6ad3fde0bdcc5104eb78c2a43232f2d2ba773a
Ashish Kumar <96014503+ashish-parjapat@users.noreply.github.com>
2022-07-31 23:13:02 +0530

    Initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Login() {
+  return (
+    <button className="form" type="submit">
+      Login
+    </button>
+  );
+}
+export default Login;
---
+++ src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div className="form">
+      <button type="submit">Register</button>
+      <input type="password" placeholder="Confirm Password" />
+    </div>
+  );
+}
+export default Register;
